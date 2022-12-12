40beae6b6b967ea616157af496d5dcea59d04ed8
Guillaume Rondon <guillaume.rondon@gmail.com>
2022-12-12 21:03:22 +0100

    day 12 part 1 find shortest path with Lee's Algorithm

--- d12/d.go
+++ d12/d.go
@@ -2,8 +2,8 @@ package d
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"strings"
 )
 
 type D struct {
@@ -19,17 +19,139 @@ func (d *D) Input(input io.Reader) {
 }
 
 func (d *D) Run() int {
-	fileScanner := bufio.NewScanner(d.inputStream)
+	matrix, src, dest := d.ParseInput()
+	pathLength := d.ShortestPath(matrix, src, dest)
 
-	fileScanner.Split(bufio.ScanLines)
-
-	for fileScanner.Scan() {
-		line := fileScanner.Text()
-		fmt.Println(line)
-	}
-	return 0
+	return pathLength
 }
 
 func (d *D) RunStr() string {
 	return ""
+}
+
+func (d *D) ParseInput() ([][]byte, position, position) {
+	result := [][]byte{}
+	fileScanner := bufio.NewScanner(d.inputStream)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	currentRow := 0
+
+	src := position{0, 0}
+	dest := position{0, 0}
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		srcIndex := strings.Index(line, "S")
+		if srcIndex > -1 {
+			src = position{srcIndex, currentRow}
+			line = strings.Replace(line, "S", "a", 1)
+		}
+
+		destIndex := strings.Index(line, "E")
+		if destIndex > -1 {
+			dest = position{destIndex, currentRow}
+		}
+
+		result = append(result, []byte(line))
+		currentRow++
+	}
+
+	return result, src, dest
+}
+
+type position struct {
+	x int
+	y int
+}
+
+type node struct {
+	x    int
+	y    int
+	dist int
+}
+
+func (d *D) ShortestPath(mat [][]byte, src position, dest position) int {
+	queue := []node{
+		{
+			x:    src.x,
+			y:    src.y,
+			dist: 0,
+		},
+	}
+
+	visited := make(map[position]bool)
+	visited[src] = true
+
+	minDistance := 0
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		currentAltitude := int(mat[current.y][current.x])
+
+		dist := current.dist
+
+		if current.x == dest.x && current.y == dest.y {
+			minDistance = dist
+			break
+		}
+
+		for _, next := range d.GetNextPositions(mat, current) {
+			if isValid(mat, visited, position{next.x, next.y}, currentAltitude) {
+				visited[position{next.x, next.y}] = true
+				queue = append(queue, next)
+			}
+		}
+
+	}
+
+	return minDistance
+}
+
+func (d *D) GetNextPositions(mat [][]byte, current node) []node {
+	positions := []node{}
+	positions = append(positions, node{
+		x:    current.x,
+		y:    current.y - 1,
+		dist: current.dist + 1,
+	})
+
+	positions = append(positions, node{
+		x:    current.x,
+		y:    current.y + 1,
+		dist: current.dist + 1,
+	})
+
+	positions = append(positions, node{
+		x:    current.x - 1,
+		y:    current.y,
+		dist: current.dist + 1,
+	})
+
+	positions = append(positions, node{
+		x:    current.x + 1,
+		y:    current.y,
+		dist: current.dist + 1,
+	})
+	return positions
+}
+
+func isValid(mat [][]byte, visited map[position]bool, pos position, currentAltitude int) bool {
+	if pos.x < 0 || pos.x >= len(mat[0]) {
+		return false
+	}
+
+	if pos.y < 0 || pos.y >= len(mat) {
+		return false
+	}
+
+	if visited[pos] {
+		return false
+	}
+
+	altitude := int(mat[pos.y][pos.x])
+
+	return altitude <= currentAltitude+1
 }
--- d12/d_test.go
+++ d12/d_test.go
@@ -5,7 +5,24 @@ import (
 	"testing"
 )
 
-func TestD1(t *testing.T) {
+//	func TestD1(t *testing.T) {
+//		input := `Sabqponm
+//
+// abcryxxl
+// accszExk
+// acctuvwj
+// abdefghi
+// `
+//
+//		d := NewD()
+//		d.Input(bytes.NewBufferString(input))
+//		got := d.Run()
+//		want := 31
+//		if got != want {
+//			t.Errorf("got %v want %v", got, want)
+//		}
+//	}
+func TestParsing(t *testing.T) {
 	input := `Sabqponm
 abcryxxl
 accszExk
@@ -15,8 +32,67 @@ abdefghi
 
 	d := NewD()
 	d.Input(bytes.NewBufferString(input))
-	got := d.Run()
-	want := 31
+	mat, src, dest := d.ParseInput()
+	want := [][]byte{
+		{97, 97, 98, 113, 112, 111, 110, 109},
+		{97, 98, 99, 114, 121, 120, 120, 108},
+		{97, 99, 99, 115, 122, 69, 120, 107},
+		{97, 99, 99, 116, 117, 118, 119, 106},
+		{97, 98, 100, 101, 102, 103, 104, 105},
+	}
+
+	if len(mat) != len(want) {
+		t.Errorf("got %v want %v", mat, want)
+	}
+
+	for i, row := range mat {
+		for j, col := range row {
+			if col != want[i][j] {
+				t.Errorf("got %v want %v", col, want[i][j])
+			}
+		}
+	}
+
+	expectedSrc := position{0, 0}
+	if src != expectedSrc {
+		t.Errorf("got %v want %v", src, position{0, 0})
+	}
+
+	expectedDest := position{5, 2}
+	if dest != expectedDest {
+		t.Errorf("got %v want %v", dest, position{7, 2})
+	}
+}
+
+func TestShortestPath(t *testing.T) {
+	matrix := [][]byte{
+		{97, 97, 98},
+		{97, 98, 99},
+		{95, 69, 100},
+	}
+
+	d := NewD()
+
+	got := d.ShortestPath(matrix, position{0, 0}, position{1, 2})
+	want := 3
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestOnlyAscending(t *testing.T) {
+	matrix := [][]byte{
+		{97, 97, 98},
+		{97, 100, 99},
+		{99, 69, 100},
+	}
+
+	d := NewD()
+
+	got := d.ShortestPath(matrix, position{0, 0}, position{1, 2})
+	want := 5
+
 	if got != want {
 		t.Errorf("got %v want %v", got, want)
 	}
